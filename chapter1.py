14f5b6c8c19185811c0b561de52d27a1799e8694
KIA-PC\Cargill Seiveright <cargill@artuvic.com>
2022-01-09 21:37:08 -0500

    CH-04

--- chapter1.py
+++ chapter1.py
@@ -22,3 +22,38 @@ def three(text):
     # Only count letters
     lst = [len((''.join(filter(str.isalpha, x)))) for x in parts]
     return lst
+
+def four(text):
+    print("04")
+    parts = text.split(" ")
+    set1, set2 = [], []
+
+    set1+= [''.join(parts[0])]
+    set2+= parts[1:5]
+
+    set1+= parts[5:10]
+    set2+= parts[10:15]
+
+    set1+= parts[15:17]
+    set2+= parts[17:19]
+
+    set1+= [''.join(parts[19])]
+    set2+= parts[20:]
+
+    result1 = [x[0] for x in set1]
+    result2 = [x[0:2] for x in set2]
+
+    j = len(parts)
+
+    s = list(range(j))
+    s1 = [s[0]] + s[5:10] + s[15:17] + [s[19]]
+    s2 = s[1:5] + s[10:15] + s[17:19] + s[20:]
+
+    result = []
+    result+= list(zip(result1, s1))
+    result+= list(zip(result2, s2))
+
+    output = {}
+
+    output = dict(result)
+    return output
--- run.py
+++ run.py
@@ -6,4 +6,15 @@ if __name__ == '__main__':
     print(chapter1.zero("hello world!")) # 00
     print(chapter1.one("schooled")) # 01
     print(chapter1.two("shoe", "cold")) # 02
-    print(chapter1.three("Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics")) # 03
+    print(
+        chapter1.three("Now I need a drink, alcoholic of course, " +
+            "after the heavy lectures involving quantum mechanics"
+        )
+    ) # 03
+    print(
+        chapter1.four(
+            "Hi He Lied Because Boron Could Not Oxidize Fluorine. " +
+            "New Nations Might Also Sign Peace Security Clause. Arthur King Can"
+        )
+    ) # 04
+    
