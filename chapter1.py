aa78e0e52de272206340dc3ced3edc927434689d
KIA-PC\Cargill Seiveright <cargill@artuvic.com>
2022-01-09 20:14:37 -0500

    Initial

---
+++ chapter1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def zero(text):
+    print("00")
+    return text[::-1]
+
+def one(text):
+    print("01")
+    return text[::2]
---
+++ run.py
@@ -0,0 +1,7 @@
+import chapter1
+
+if __name__ == '__main__':
+    # Chapter 1
+    print("\n\n******************Chapter 1******************\n\n")
+    print(chapter1.zero("hello world!")) # 00
+    print(chapter1.one("schooled")) # 01
